12151f25613123bb5ea1e9a9dcb0536872c3302f
unknown <islammohamedahmed49@gmail.com>
2024-03-16 18:30:24 +0200

    bear big brother

---
+++ BearAndBigBrother/Program.cs
@@ -0,0 +1,21 @@
+﻿using static System.Console;
+using static System.Convert;
+using static System.Math;
+
+int a, b, counter = 0;
+
+
+string str = ReadLine();
+var arr = str.Split(" ");
+a = ToInt32(arr[0]);
+b = ToInt32(arr[1]);
+while (a <= b)
+{
+    a *= 3;
+    b *= 2;
+    counter++;
+}
+
+WriteLine(counter);
+
+
